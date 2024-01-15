e357e43fc765be64d48cac5910fa53e29dd2e9fc
Andrei Fajardo <andrei@nerdai.io>
2024-01-15 01:25:49 -0500

    hello world of rust extension module

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
+use pyo3::prelude::*;
+
+#[pyfunction]
 pub fn add(left: usize, right: usize) -> usize {
     left + right
+}
+
+#[pymodule]
+#[pyo3(name = "_rust")]
+fn module_extension(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(add, m)?)?;
+    Ok(())
 }
 
 #[cfg(test)]
