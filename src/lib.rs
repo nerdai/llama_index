30336598925422fac45c115df5a57c0237d08b8d
Andrei Fajardo <andrei@nerdai.io>
2024-01-15 09:56:33 -0500

    add typed func signatures for merge_splits

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,22 @@
 use pyo3::prelude::*;
 
 #[pyfunction]
-pub fn add(left: usize, right: usize) -> usize {
+fn add(left: usize, right: usize) -> usize {
     left + right
+}
+
+#[pyfunction]
+fn _merge_splits(splits: Vec<(&str, bool)>, chunk_size: usize) -> Vec<String> {
+    println!("{:?}", splits);
+    println!("{:?}", chunk_size);
+    vec![]
 }
 
 #[pymodule]
 #[pyo3(name = "_rust")]
 fn module_extension(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(add, m)?)?;
+    m.add_function(wrap_pyfunction!(_merge_splits, m)?)?;
     Ok(())
 }
 
