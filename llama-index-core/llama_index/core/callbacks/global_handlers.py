45fcb81d432394c7a126672993543eacf14f2562
Andrei Fajardo <andrei@nerdai.io>
2024-01-17 12:14:03 -0500

    adjust global handler to simple LLM

--- llama-index-core/llama_index/core/callbacks/global_handlers.py
+++ llama-index-core/llama_index/core/callbacks/global_handlers.py
@@ -2,18 +2,8 @@
 
 from typing import Any
 
-from llama_index.core.callbacks.arize_phoenix_callback import (
-    arize_phoenix_callback_handler,
-)
 from llama_index.core.callbacks.base_handler import BaseCallbackHandler
-from llama_index.core.callbacks.deepeval_callback import deepeval_callback_handler
-from llama_index.core.callbacks.honeyhive_callback import honeyhive_callback_handler
-from llama_index.core.callbacks.open_inference_callback import (
-    OpenInferenceCallbackHandler,
-)
-from llama_index.core.callbacks.promptlayer_handler import PromptLayerHandler
 from llama_index.core.callbacks.simple_llm_handler import SimpleLLMHandler
-from llama_index.core.callbacks.wandb_callback import WandbCallbackHandler
 
 
 def set_global_handler(eval_mode: str, **eval_params: Any) -> None:
@@ -25,21 +15,4 @@ def set_global_handler(eval_mode: str, **eval_params: Any) -> None:
 
 def create_global_handler(eval_mode: str, **eval_params: Any) -> BaseCallbackHandler:
     """Get global eval handler."""
-    if eval_mode == "wandb":
-        handler: BaseCallbackHandler = WandbCallbackHandler(**eval_params)
-    elif eval_mode == "openinference":
-        handler = OpenInferenceCallbackHandler(**eval_params)
-    elif eval_mode == "arize_phoenix":
-        handler = arize_phoenix_callback_handler(**eval_params)
-    elif eval_mode == "honeyhive":
-        handler = honeyhive_callback_handler(**eval_params)
-    elif eval_mode == "promptlayer":
-        handler = PromptLayerHandler(**eval_params)
-    elif eval_mode == "deepeval":
-        handler = deepeval_callback_handler(**eval_params)
-    elif eval_mode == "simple":
-        handler = SimpleLLMHandler(**eval_params)
-    else:
-        raise ValueError(f"Eval mode {eval_mode} not supported.")
-
-    return handler
+    return SimpleLLMHandler(**eval_params)
