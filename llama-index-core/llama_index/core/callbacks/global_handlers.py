03eeb16adda5738f102b4a9de83f3cf368916581
Andrei Fajardo <andrei@nerdai.io>
2024-01-19 16:29:43 -0500

    refactor global_handlers

--- llama-index-core/llama_index/core/callbacks/global_handlers.py
+++ llama-index-core/llama_index/core/callbacks/global_handlers.py
@@ -1,18 +1,23 @@
 """Global eval handlers."""
 
-from typing import Any
+from typing import Any, Optional, Protocol
 
 from llama_index.core.callbacks.base_handler import BaseCallbackHandler
 from llama_index.core.callbacks.simple_llm_handler import SimpleLLMHandler
 
 
-def set_global_handler(eval_mode: str, **eval_params: Any) -> None:
+class HandlerCallable(Protocol):
+    def __call__(self, **eval_params: Any) -> BaseCallbackHandler:
+        ...
+
+
+def set_global_handler(
+    handler_callable: Optional[HandlerCallable], **eval_params: Any
+) -> None:
     """Set global eval handlers."""
     import llama_index
 
-    llama_index.core.global_handler = create_global_handler(eval_mode, **eval_params)
-
-
-def create_global_handler(eval_mode: str, **eval_params: Any) -> BaseCallbackHandler:
-    """Get global eval handler."""
-    return SimpleLLMHandler(**eval_params)
+    if handler_callable is None:
+        llama_index.core.global_handler = SimpleLLMHandler(**eval_params)
+    else:
+        llama_index.core.global_handler = handler_callable(**eval_params)
