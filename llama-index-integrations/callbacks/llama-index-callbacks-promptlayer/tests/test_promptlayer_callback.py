4661868b1430aa6322db9242b38863718c803079
Andrei Fajardo <andrei@nerdai.io>
2024-01-21 14:15:23 -0500

    promptlayer readme

--- llama-index-integrations/callbacks/llama-index-callbacks-promptlayer/tests/test_promptlayer_callback.py
+++ llama-index-integrations/callbacks/llama-index-callbacks-promptlayer/tests/test_promptlayer_callback.py
@@ -1,7 +1,7 @@
 from llama_index.core.callbacks.base_handler import BaseCallbackHandler
-from llama_index.callbacks.openinference.base import OpenInferenceCallbackHandler
+from llama_index.callbacks.promptlayer.base import PromptLayerHandler
 
 
 def test_handler_callable():
-    handler = OpenInferenceCallbackHandler()
+    handler = PromptLayerHandler()
     assert isinstance(handler, BaseCallbackHandler)
