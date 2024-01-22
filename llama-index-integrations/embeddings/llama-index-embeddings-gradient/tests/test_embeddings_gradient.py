4712d411da089d9e84abf9f8c574b814c88a49ee
Andrei Fajardo <andrei@nerdai.io>
2024-01-22 15:01:18 -0500

    gradient

---
+++ llama-index-integrations/embeddings/llama-index-embeddings-gradient/tests/test_embeddings_gradient.py
@@ -0,0 +1,9 @@
+def test_embedding_class():
+    """TODO: mock this"""
+    # emb = GradientEmbedding(
+    #     gradient_model_slug="fake_slug",
+    #     gradient_access_token="fake_at",
+    #     gradient_workspace_id="fake_workspace_id",
+    # )
+    # assert isinstance(emb, BaseEmbedding)
+    pass
