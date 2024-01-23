7154f86380ea3f6142fb92592958b4e69b238e93
Andrei Fajardo <andrei@nerdai.io>
2024-01-23 17:03:03 -0500

    openai rip out azure utils

--- llama-index-integrations/llms/llama-index-llms-openai/llama_index/llms/openai/utils.py
+++ llama-index-integrations/llms/llama-index-llms-openai/llama_index/llms/openai/utils.py
@@ -1,6 +1,5 @@
 import logging
 import os
-import time
 from typing import Any, Callable, Dict, List, Optional, Sequence, Tuple, Type, Union
 
 import openai
@@ -331,45 +330,6 @@ def resolve_openai_credentials(
     final_api_version = api_version or openai.api_version or DEFAULT_OPENAI_API_VERSION
 
     return final_api_key, str(final_api_base), final_api_version
-
-
-def refresh_openai_azuread_token(
-    azure_ad_token: Any = None,
-) -> Any:
-    """
-    Checks the validity of the associated token, if any, and tries to refresh it
-    using the credentials available in the current context. Different authentication
-    methods are tried, in order, until a successful one is found as defined at the
-    package `azure-indentity`.
-    """
-    try:
-        from azure.core.exceptions import ClientAuthenticationError
-        from azure.identity import DefaultAzureCredential
-    except ImportError as ex:
-        raise ValueError(
-            "Using API type `azure_ad` or `azuread` requires the package"
-            " `azure-identity` to be installed."
-        ) from ex
-
-    if not azure_ad_token or azure_ad_token.expires_on < time.time() + 60:
-        try:
-            credential = DefaultAzureCredential()
-            azure_ad_token = credential.get_token(
-                "https://cognitiveservices.azure.com/.default"
-            )
-        except ClientAuthenticationError as err:
-            raise ValueError(
-                "Unable to acquire a valid Microsoft Entra ID (former Azure AD) token for "
-                f"the resource due to the following error: {err.message}"
-            ) from err
-    return azure_ad_token
-
-
-def resolve_from_aliases(*args: Optional[str]) -> Optional[str]:
-    for arg in args:
-        if arg is not None:
-            return arg
-    return None
 
 
 def validate_openai_api_key(api_key: Optional[str] = None) -> None:
