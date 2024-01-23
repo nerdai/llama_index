b5510cdb602737905aada5ace8b24426bf392929
Andrei Fajardo <andrei@nerdai.io>
2024-01-23 14:13:49 -0500

    huggingface

---
+++ llama-index-integrations/embeddings/llama-index-embeddings-huggingface/llama_index/embeddings/huggingface/__init__.py
@@ -0,0 +1,10 @@
+from llama_index.embeddings.huggingface.base import (
+    HuggingFaceInferenceAPIEmbedding,
+    HuggingFaceInferenceAPIEmbeddings,
+)
+
+__all__ = [
+    "HuggingFaceembedding",
+    "HuggingFaceInferenceAPIEmbedding",
+    "HuggingFaceInferenceAPIEmbeddings",
+]
