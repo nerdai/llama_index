530101535f629c198428fdf5dc2e777a2a964880
Andrei Fajardo <andrei@nerdai.io>
2024-01-27 22:12:10 -0500

    files from llama-index

---
+++ llama-index-integrations/readers/llama-index-readers-file/llama_index/readers/file/__init__.py
@@ -0,0 +1,30 @@
+from llama_index.core.readers.file.docs_reader import DocxReader, HWPReader, PDFReader
+from llama_index.core.readers.file.epub_reader import EpubReader
+from llama_index.core.readers.file.flat_reader import FlatReader
+from llama_index.core.readers.file.image_caption_reader import ImageCaptionReader
+from llama_index.core.readers.file.image_reader import ImageReader
+from llama_index.core.readers.file.image_vision_llm_reader import ImageVisionLLMReader
+from llama_index.core.readers.file.ipynb_reader import IPYNBReader
+from llama_index.core.readers.file.markdown_reader import MarkdownReader
+from llama_index.core.readers.file.mbox_reader import MboxReader
+from llama_index.core.readers.file.slides_reader import PptxReader
+from llama_index.core.readers.file.tabular_reader import PandasCSVReader
+from llama_index.core.readers.file.video_audio_reader import VideoAudioReader
+
+__all__ = [
+    "DocxReader",
+    "HWPReader",
+    "PDFReader",
+    "EpubReader",
+    "FlatReader",
+    "ImageCaptionReader",
+    "ImageReader",
+    "ImageVisionLLMReader",
+    "IPYNBReader",
+    "IPYNBReader",
+    "MarkdownReader",
+    "MboxReader",
+    "PptxReader",
+    "PandasCSVReader",
+    "VideoAudioReader",
+]
