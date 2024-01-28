8ba2427bb95b06a26527fdac29cd4746d429252e
Andrei Fajardo <andrei@nerdai.io>
2024-01-27 22:40:16 -0500

    file

--- llama-index-integrations/readers/llama-index-readers-file/llama_index/readers/file/__init__.py
+++ llama-index-integrations/readers/llama-index-readers-file/llama_index/readers/file/__init__.py
@@ -1,15 +1,16 @@
-from llama_index.core.readers.file.docs_reader import DocxReader, HWPReader, PDFReader
-from llama_index.core.readers.file.epub_reader import EpubReader
-from llama_index.core.readers.file.flat_reader import FlatReader
-from llama_index.core.readers.file.image_caption_reader import ImageCaptionReader
-from llama_index.core.readers.file.image_reader import ImageReader
-from llama_index.core.readers.file.image_vision_llm_reader import ImageVisionLLMReader
-from llama_index.core.readers.file.ipynb_reader import IPYNBReader
-from llama_index.core.readers.file.markdown_reader import MarkdownReader
-from llama_index.core.readers.file.mbox_reader import MboxReader
-from llama_index.core.readers.file.slides_reader import PptxReader
-from llama_index.core.readers.file.tabular_reader import PandasCSVReader
-from llama_index.core.readers.file.video_audio_reader import VideoAudioReader
+from llama_index.readers.file.docs.base import DocxReader, HWPReader, PDFReader
+from llama_index.readers.file.epub.base import EpubReader
+from llama_index.readers.file.flat.base import FlatReader
+from llama_index.readers.file.image_caption.base import ImageCaptionReader
+from llama_index.readers.file.image.base import ImageReader
+from llama_index.readers.file.image_vision_llm.base import ImageVisionLLMReader
+from llama_index.readers.file.ipynb.base import IPYNBReader
+from llama_index.readers.file.markdown.base import MarkdownReader
+from llama_index.readers.file.mbox.base import MboxReader
+from llama_index.readers.file.slides.base import PptxReader
+from llama_index.readers.file.tabular.base import PandasCSVReader
+from llama_index.readers.file.video_audio.base import VideoAudioReader
+
 
 __all__ = [
     "DocxReader",
@@ -20,7 +21,6 @@ __all__ = [
     "ImageCaptionReader",
     "ImageReader",
     "ImageVisionLLMReader",
-    "IPYNBReader",
     "IPYNBReader",
     "MarkdownReader",
     "MboxReader",
