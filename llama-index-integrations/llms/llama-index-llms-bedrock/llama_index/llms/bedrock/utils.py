b05dd1c0c79d1b24a3b982d964b62b1140f3f8e7
Andrei Fajardo <andrei@nerdai.io>
2024-01-30 13:01:44 -0500

    update bedrock

--- llama-index-integrations/llms/llama-index-llms-bedrock/llama_index/llms/bedrock/utils.py
+++ llama-index-integrations/llms/llama-index-llms-bedrock/llama_index/llms/bedrock/utils.py
@@ -1,5 +1,4 @@
 import logging
-import boto3  # noqa
 from abc import ABC, abstractmethod
 from typing import Any, Callable, Optional, Sequence
 
@@ -165,6 +164,14 @@ def _create_retry_decorator(client: Any, max_retries: int) -> Callable[[Any], An
     max_seconds = 10
     # Wait 2^x * 1 second between each retry starting with
     # 4 seconds, then up to 10 seconds, then 10 seconds afterwards
+    try:
+        import boto3  # noqa
+    except ImportError as e:
+        raise ImportError(
+            "You must install the `boto3` package to use Bedrock."
+            "Please `pip install boto3`"
+        ) from e
+
     return retry(
         reraise=True,
         stop=stop_after_attempt(max_retries),
