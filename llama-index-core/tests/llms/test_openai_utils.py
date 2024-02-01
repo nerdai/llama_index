61e1578f24d82d26c0984cf25fbbeb992fa9a971
Andrei Fajardo <andrei@nerdai.io>
2024-02-01 02:01:55 -0500

    rip out multi-modal llms

--- llama-index-core/tests/llms/test_openai_utils.py
+++ llama-index-core/tests/llms/test_openai_utils.py
@@ -2,7 +2,7 @@ from typing import List
 
 import pytest
 from llama_index.core.bridge.pydantic import BaseModel
-from llama_index.core.llms.openai_utils import (
+from llama_index.core.embeddings.openai_utils import (
     from_openai_message_dicts,
     from_openai_messages,
     to_openai_message_dicts,
